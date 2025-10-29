cebc43190bfea39267c3ae575c08f024b1bdd7c9
daveFromGrogtag <dave@grogtag.com>
2025-10-29 10:17:23 -0700

    revised changes to the url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://davefromgrogtag.github.io/new-pangaea-print-site/',
+    site: 'https://davefromgrogtag.github.io/',
     base: "/new-pangaea-print-site/"
 });
